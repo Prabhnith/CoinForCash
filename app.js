b9545a4ce7290dcc09e467ec8f7b290d99828a9f
Prabhjot Singh 2 <prasumma@publicisgroupe.net>
2018-04-25 21:03:47 +0530

    timer added

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,9 +18,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get("/",function(req,res){
+  res.render("index")
+});
+
+app.get("/users",function(req,res){
+  res.render("users")
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
