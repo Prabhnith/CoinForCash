daaec5886bd92b7bcc602691103fc42773d312e9
prabhnith <prabhjot.nith@gmail.com>
2018-04-28 23:54:53 +0530

    Winner collection added

---
+++ models/Winners.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var WinnersSchema = new Schema({
+  mobile: { type: Number, required: true },
+  amount: { type: Number, required: true },
+  email: { type: String, required: true },
+  date_created: { type: Date, default: Date.now } 
+});
+
+var Winners = mongoose.model('Winners', WinnersSchema);
+
+module.exports = Winners;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var Winners = require('../models/Winners');
 
 router.get('/', function(req, res, next) {
-  
+
+  var Winner = Winners({
+    mobile: 9805830800,
+    amount: 300,
+    email: "prabhjot.nith@gmail.com"
+  });
+
+  Winner.save(function (err) {
+    console.log("Winner Adding.....");
+    if(err) throw err;
+    console.log("Winner Added");
+  });
+
   User.find({}, function (err, users) {
     if (err) throw err;
 
