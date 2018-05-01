01d74f0b2b2be61042378a5fa240cc0654cc8b04
prabhnith <prabhjot.nith@gmail.com>
2018-05-01 18:07:00 +0530

    config file added

---
+++ config.js
@@ -0,0 +1,15 @@
+var config = {
+
+    auth: {
+        user: process.env.GMAIL_ID,
+        pass: process.env.GMAIL_PASS
+    },
+
+    mailOptions : {
+        from: 'prabhjot.nith@gmail.com', // sender address
+        to: "", // list of receivers
+        subject: 'Coin For Cash - Thanks for participating', // Subject line
+        html: '<img src="https://raw.githubusercontent.com/Prabhnith/CoinForCash/master/public/images/bestWishes.jpg" alt="best Wishes for the event!!">'
+    }
+};
+module.exports = config;
